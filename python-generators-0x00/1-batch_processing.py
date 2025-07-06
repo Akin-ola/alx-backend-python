2bd0b0e2feb30459ed0a2327c0fc33932ed3b9ac
Akin-ola <idris_akinola@yahoo.com>
2025-07-06 22:57:21 +0100

    Added script to 1-batch processes file

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,47 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+
+
+def stream_users_in_batches(batch_size):
+    """
+    Generator that return batches from user_data table.
+    """
+
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database="ALX_prodev"
+        )
+
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+
+        while True:
+            batch = cursor.fetchmany(batch_size)
+            if not batch:
+                break
+            yield batch
+
+    except Error as e:
+        print(f"[ERROR] {e}")
+
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'connection' in locals():
+            connection.close()
+
+
+def batch_processing(batch_size):
+    """
+    Generator that yields users older than 25, one by one, from batch streams.
+    """
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
+            if user["age"] > 25:
+                print(user)
+                yield user
