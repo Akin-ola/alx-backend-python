25ed6a64f080e28c24b5e26ddfc278ee0c18a747
Akin-ola <idris_akinola@yahoo.com>
2025-07-06 23:11:20 +0100

    i write script in stream ages file to calculate the average of streamers

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,25 @@
+from seed import connect_to_prodev
+
+def stream_user_ages():
+    conn = connect_to_prodev()
+    cursor = conn.cursor()
+    cursor.execute("SELECT age FROM user_data")
+    for (age,) in cursor:
+        yield age
+    cursor.close()
+    conn.close()
+
+def calculate_average_age():
+    total_age = 0
+    count = 0
+    for age in stream_user_ages():
+        total_age += float(age)
+        count += 1
+    if count > 0:
+        avg = total_age / count
+        print(f"Average age of users: {avg:.2f}")
+    else:
+        print("No users found.")
+
+if __name__ == "__main__":
+    calculate_average_age()
