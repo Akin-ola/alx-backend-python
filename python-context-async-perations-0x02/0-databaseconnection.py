7acca8b856fe000ef467b9b4841f4bf4d70201ad
Akin-ola <idris_akinola@yahoo.com>
2025-07-30 18:35:38 +0100

    I created a context manager that create connection to the database and close the connection after activity

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,30 @@
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = {
+    'host': os.getenv('DB_HOST'),
+    'user': os.getenv('DB_USER'),
+    'database': os.getenv('DB_NAME'),
+    'password': os.getenv('DB_PASSWORD'),
+}
+class Database_connection:
+    def __enter__(self):
+        self.conn = mysql.connector.connect(**config)
+        self.cursor = self.conn.cursor()
+        print ("Connection Created...")
+        return self.cursor
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+        print("Connection Closed...")
+
+
+with Database_connection() as cursor:
+    cursor.execute("SELECT * FROM user_data")
+    result = cursor.fetchall()
+    print(result)
