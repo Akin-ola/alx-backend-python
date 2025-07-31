d8363531b73a2b8b81a1a6e76c6e5b5ba8a3c052
Akin-ola <idris_akinola@yahoo.com>
2025-07-31 05:09:12 +0100

    I rename a function

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -15,7 +15,7 @@ async def async_fetch_older_users():
         return rows
 
 
-async def main():
+async def fetch_concurrently():
     users, older_users = await asyncio.gather(
         async_fetch_users(), 
         async_fetch_older_users()
@@ -24,4 +24,4 @@ async def main():
     print("Older Users:", older_users)
 
 
-asyncio.run(main())
+asyncio.run(fetch_concurrently())
